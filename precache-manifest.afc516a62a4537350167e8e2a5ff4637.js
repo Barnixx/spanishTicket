755b65b3be2be1e2a44cf9de9943f0efe735e299
Barnixx <rb.kuszaj@gmail.com>
2019-05-15 19:28:35 +0200

    Updates

---
+++ precache-manifest.afc516a62a4537350167e8e2a5ff4637.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25b2dcdd2a54ec3c23c526a1e9f7a7ea",
+    "url": "/spanishTicket/index.html"
+  },
+  {
+    "revision": "f90863bd1592bf18bf7e",
+    "url": "/spanishTicket/static/css/2.651fcabf.chunk.css"
+  },
+  {
+    "revision": "014920b303b883241f26",
+    "url": "/spanishTicket/static/css/main.3c84c83d.chunk.css"
+  },
+  {
+    "revision": "f90863bd1592bf18bf7e",
+    "url": "/spanishTicket/static/js/2.586d348e.chunk.js"
+  },
+  {
+    "revision": "014920b303b883241f26",
+    "url": "/spanishTicket/static/js/main.898e020a.chunk.js"
+  },
+  {
+    "revision": "656b6c6de1e51c186c74",
+    "url": "/spanishTicket/static/js/runtime~main.999979d0.js"
+  },
+  {
+    "revision": "2ef8ba3410dcc71578a880e7064acd7a",
+    "url": "/spanishTicket/static/media/fa-brands-400.2ef8ba34.woff"
+  },
+  {
+    "revision": "5bf145531213545e03ff41cd27df7d2b",
+    "url": "/spanishTicket/static/media/fa-brands-400.5bf14553.svg"
+  },
+  {
+    "revision": "5e2f92123d241cabecf0b289b9b08d4a",
+    "url": "/spanishTicket/static/media/fa-brands-400.5e2f9212.woff2"
+  },
+  {
+    "revision": "98b6db59be947f563350d2284fc9ea36",
+    "url": "/spanishTicket/static/media/fa-brands-400.98b6db59.ttf"
+  },
+  {
+    "revision": "a7b95dbdd87e0c809570affaf366a434",
+    "url": "/spanishTicket/static/media/fa-brands-400.a7b95dbd.eot"
+  },
+  {
+    "revision": "427d721b86fc9c68b2e85ad42b69238c",
+    "url": "/spanishTicket/static/media/fa-regular-400.427d721b.woff"
+  },
+  {
+    "revision": "5eb754ab7dbd2fee562360528db4c3c0",
+    "url": "/spanishTicket/static/media/fa-regular-400.5eb754ab.svg"
+  },
+  {
+    "revision": "65b9977aa23185e8964b36eddbce7a20",
+    "url": "/spanishTicket/static/media/fa-regular-400.65b9977a.ttf"
+  },
+  {
+    "revision": "dcce4b7fbd5e895561e18af4668265af",
+    "url": "/spanishTicket/static/media/fa-regular-400.dcce4b7f.eot"
+  },
+  {
+    "revision": "e6257a726a0cf6ec8c6fec22821c055f",
+    "url": "/spanishTicket/static/media/fa-regular-400.e6257a72.woff2"
+  },
+  {
+    "revision": "418dad87601f9c8abd0e5798c0dc1feb",
+    "url": "/spanishTicket/static/media/fa-solid-900.418dad87.woff2"
+  },
+  {
+    "revision": "46e7cec623d8bd790d9fdbc8de2d3ee7",
+    "url": "/spanishTicket/static/media/fa-solid-900.46e7cec6.eot"
+  },
+  {
+    "revision": "49279363201ed19a840796e05a74a91b",
+    "url": "/spanishTicket/static/media/fa-solid-900.49279363.svg"
+  },
+  {
+    "revision": "a7140145ebaaf5fb14e40430af5d25c4",
+    "url": "/spanishTicket/static/media/fa-solid-900.a7140145.woff"
+  },
+  {
+    "revision": "ff8d9f8adb0d09f11d4816a152672f53",
+    "url": "/spanishTicket/static/media/fa-solid-900.ff8d9f8a.ttf"
+  },
+  {
+    "revision": "f2283dc4175e2a2cb5a7d829a4c5408d",
+    "url": "/spanishTicket/static/media/hero_image.f2283dc4.jpg"
+  },
+  {
+    "revision": "7f191ce3b2632246c7b86816446f5cfc",
+    "url": "/spanishTicket/static/media/pinchos.7f191ce3.jpg"
+  },
+  {
+    "revision": "08ba1cd0701c05e7b40bf6c7d8009644",
+    "url": "/spanishTicket/static/media/spanish_ticket_black.08ba1cd0.png"
+  }
+]);
